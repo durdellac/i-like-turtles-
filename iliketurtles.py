8e44220998d8de4cd18b43b88876aa8a0ac40c51
durdellac <coledurdella@gmail.com>
2018-09-17 15:10:53 -0400

    forward and stop

--- iliketurtles.py
+++ iliketurtles.py
@@ -1,13 +1,8 @@
-from turtle import Turtle
-from math import cos
+from gopigo import*
+import time
 
-t = Turtle()
-t.shape("turtle")
+fwd()
 
+time.sleep(5)
 
-for x in range(50):
-    t.pensize(100*cos(1))
-    t.forward(10*x)
-    t.right(40)
-
-input("Press any key to exit")
+stop()
